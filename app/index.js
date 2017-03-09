e1486bca93f2f6bd84a301f2f555a4cb74deb8ea
slashhuang <627284482@qq.com>
2017-03-09 09:23:41 +0800

    change to promise

--- app/index.js
+++ app/index.js
@@ -8,9 +8,6 @@
  class APP {
  	initServer(){
  		return (request,response)=>{
- 			request.on("data",(chunked)=>{
- 				console.log('getting data',chunked)
- 			})
  			//静态资源处理
  			StaticHandler(request,response).then((data)=>{
  				response.end(data)
--- index.js
+++ index.js
@@ -9,6 +9,6 @@
  /*主程序逻辑*/
  const App = require('./app');
  let handlerInstance = new App()
- http.createServer(handlerInstance.initServer()).listen(PORT);
-
- console.log(`node程序开始监听端口: ${PORT}`)
+ http.createServer(handlerInstance.initServer()).listen(PORT,()=>{
+ 	 console.log(`node----程序开始监听端口: ${PORT}`)
+ });
