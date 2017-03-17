a4468c4b42bd26bc9dfff04883197d9da25b8cd3
slashhuang <627284482@qq.com>
2017-03-18 01:52:21 +0800

    add converter for markdown

--- public/js/write.js
+++ public/js/write.js
@@ -3,9 +3,32 @@
  * @Author slashhuang
  */
 
+
+require('./write.scss');
 const SimpleMDE = require('simplemde');
+const showdown  = require('showdown');
+
+let app = {
+	init:function(){
+		setTimeout(()=>{
+			$('#simplemde').removeClass('invisible')
+		},0);
+		this.SimpleMDE = new SimpleMDE();
+		this.converter =  new showdown.Converter();
+		this.events()
+	},
+	makeHtml:function(markdown){
+		return this.converter.makeHtml(markdown)
+	},
+	events:function(){
+		$('.submit-btn').click(()=>{
+			let markdown = this.SimpleMDE.value();
+			let html = this.makeHtml(markdown);
+			$('#preview').html(html)
+		});
+	}
+};
+app.init()
 
 
-let SimpleMDE_inst = new SimpleMDE();
 
-console.log('fuck')
